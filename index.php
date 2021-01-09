1d683fbf267dd63720fed05bb3ec8325302e7aff
toebay007 <toebay007@gmail.com>
2021-01-09 02:56:08 +0100

    Day 1(Index page drafting)
    Day 1 for web assessment
    
    Co-Authored-By: Bond <4683867+bondz@users.noreply.github.com>
    Co-Authored-By: dreplica <27150392+dreplica@users.noreply.github.com>
    Co-Authored-By: Uche Uzochukwu Mark  <50272213+markeu@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>DevPlacement Assessment</title>
+        <!-- custom css -->
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <!-- bootstrap 5.0.0 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row rowBd">
+                <div class="col-md-5 offset-md-1 firstDiv">
+                    <div class="col-md-12 firstDivs"></div>
+                    <div class="col-md-12 firstDivs">
+                        <h3>HELLO, <strong>EMERALD</strong></h3>
+                        <p>
+                            Welcome to your dashboard, Kindly sort through the user base
+                        </p>
+                        <div class="col-md-8 offset-md-2">
+                            <input type="search" class="inputStyle" id="specificSizeInputName" placeholder="Jane Doe">
+                        </div>
+                    </div>
+                    <div class="col-md-12 firstDivs"></div>
+                </div>
+                <div class="col-md-4 offset-md-1 firstDiv" id="SecondDiv">
+                    
+                </div>
+            </div>
+        </div>
+
+        <!-- footer -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+           
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+            <script type="text/javascript" src="style.js"></script>
+    </body>
+</html>
