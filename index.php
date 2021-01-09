11d627fb2c433adcf0c73fad90dcc91ea9a9621b
toebay007 <toebay007@gmail.com>
2021-01-09 19:14:11 +0100

    completion of Left design
    Completion of Day 1
    
    Co-Authored-By: Bond <4683867+bondz@users.noreply.github.com>
    Co-Authored-By: dreplica <27150392+dreplica@users.noreply.github.com>
    Co-Authored-By: Uche Uzochukwu Mark  <50272213+markeu@users.noreply.github.com>

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
         <link rel="stylesheet" type="text/css" href="style.css">
         <!-- bootstrap 5.0.0 -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <!-- font awesome link -->
+        <link href="FA/css/all.css" rel="stylesheet">
     </head>
     <body>
         <div class="container-fluid">
@@ -18,12 +21,47 @@
                         <p>
                             Welcome to your dashboard, Kindly sort through the user base
                         </p>
-                        <div class="col-md-8 offset-md-2">
-                            <input type="search" class="inputStyle" id="specificSizeInputName" placeholder="Jane Doe">
+                        <div class="col-md-8 offset-md-2 boxs">
+                            <a href="#"><i class="bi bi-search"></i></a>
+                            <input type="text" id="specificSizeInputName" placeholder="Find a User">
                         </div>
                     </div>
-                    <div class="col-md-12 firstDivs"></div>
+                    <div class="col-md-12 firstDivs">
+                        <p>Show users</p>
+                        <div class="row">
+                            <div class="col-md-4 userStyle">
+                                <a href="#">
+                                    <div class="col-12 text-center hoves" id="usersStyle">
+                                        <i class="fas fa-users fa-3x"></i>
+                                    </div>
+                                </a>
+                                <p class="userP">All Users</p>
+                               </div>
+                            <div class="col-md-4 userStyle">
+                                <a href="#">
+                                    <div class="col-12 text-center hoves" id="maleStyle">
+                                    <i class="fas fa-male fa-3x"></i>
+                                    </div>
+                                      </a>
+                                <p class="userP">Male Users</p>
+                              </div>
+                            <div class="col-md-4 userStyle">
+                                <a href="#">
+                                    <div class="col-12 text-center hoves" id="femaleStyle">
+                                    <i class="fas fa-female fa-3x"></i>
+                                    </div>
+                                       </a>
+                                <p class="userP">Female Users</p>
+                             </div>
+                        </div>
+                    </div>
                 </div>
+
+
+
+
+
+
                 <div class="col-md-4 offset-md-1 firstDiv" id="SecondDiv">
                     
                 </div>
